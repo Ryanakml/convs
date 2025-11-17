6c643e19883663494fe501fcfecc1dc19abe2f67
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-11-17 13:13:14 +0700

    04: Organizations fixed and improved - reference coderabbit

--- apps/web/modules/auth/ui/layouts/auth-layouts.tsx
+++ apps/web/modules/auth/ui/layouts/auth-layouts.tsx
@@ -1,6 +1,6 @@
 export const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col items-center h-full min-h-screen min-w-screen justify-center">
+    <div className="flex flex-col items-center h-full min-h-screen min-w-full justify-center">
       {children}
     </div>
   );
--- apps/web/modules/auth/ui/views/orgs-select-view.tsx
+++ apps/web/modules/auth/ui/views/orgs-select-view.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { OrganizationList } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
 
 export const OrgsSelectionView = () => {
+  const params = useSearchParams();
+  const redirectUrl = params.get("redirectUrl") || "/";
   return (
     <OrganizationList
-      afterCreateOrganizationUrl="/"
-      afterSelectOrganizationUrl="/"
+      afterCreateOrganizationUrl={redirectUrl}
+      afterSelectOrganizationUrl={redirectUrl}
       hidePersonal
       skipInvitationScreen
     />
