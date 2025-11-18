4a722733a47e5df0fce8ca349091149dd7ccaa14
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-11-18 16:01:18 +0700

    07: Dashboard Layouts

---
+++ apps/web/app/(dashboard)/customization/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CustomizationPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Customization</h1>
+      </div>
+    </div>
+  );
+};
+export default CustomizationPage;
