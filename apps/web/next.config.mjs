1fa9a0256e37e6cf9ba379a9c1a1fc1a78928fcb
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-12-05 17:28:11 +0700

    misspelled

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "conversation",
+        destination: "conversations",
         permanent: false,
       },
     ];
